725bb79256b4167d273b1971875dad8420ec6af2
paras18csu153 <46554048+paras18csu153@users.noreply.github.com>
2020-04-15 08:35:05 +0530

    Update cashier.route.js

--- routes/cashier.route.js
+++ routes/cashier.route.js
@@ -4,3 +4,4 @@ const cashier_controller = require("../controllers/cashier.controller")
 router.post("/viewBills", cashier_controller.viewBills)
 router.post("/generateBill", cashier_controller.generateBill)
 router.post("/modeofPayment", cashier_controller.modeofPayment)
+module.exports = router;
