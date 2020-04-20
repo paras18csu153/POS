511f1eb15994be2a74c55c5df9ba1c4e00d5923b
marora10 <46513213+marora10@users.noreply.github.com>
2020-04-20 13:22:08 +0530

    muskan's contribution for the code

--- routes/waiter.route.js
+++ routes/waiter.route.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const router = express.Router()
 const waiter_controller = require("../controllers/waiter.controller")
-router.get("/Tables", waiter_controller.viewTables)
-router.post("/Status", waiter_controller.updateStatus)
-router.get("/Menu", waiter_controller.viewMenu)
-router.get("/Order", waiter_controller.viewOrder)
-router.post("/Order", waiter_controller.takeOrder)
-router.post("/Order", waiter_controller.updateOrder)
-router.post("/OTP", waiter_controller.generateOTP)
-router.post("/Customerdetails", waiter_controller.updateCustomerdetails)
+router.get("/tables", waiter_controller.viewTables)
+router.post("/status", waiter_controller.updateStatus)// extra feature
+router.get("/menu", waiter_controller.viewMenu)
+router.get("/order", waiter_controller.viewOrder)
+router.post("/order", waiter_controller.takeOrder)
+router.put("/order", waiter_controller.updateOrder)
+router.post("/otp", waiter_controller.generateOTP)// extra feature
+router.post("/bill", waiter_controller.generateBill)
+//router.post("/Customer", waiter_controller.updateCustomerdetails)//extra feature
 module.exports = router;
