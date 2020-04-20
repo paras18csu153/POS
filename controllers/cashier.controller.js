a86a781025e7d96b3339b851b502e420a7f7526b
chirgmalik <46554055+chirgmalik@users.noreply.github.com>
2020-04-20 13:34:21 +0530

    chirag's contribution

--- controllers/cashier.controller.js
+++ controllers/cashier.controller.js
@@ -1,11 +1,27 @@
+const viewBills = require('../models/view.models')
+const table = require('../models/table.model')
+
+
+
 exports.viewBills = async (req, res) => {
+    var bill = await viewBills.find()
+    res.status(200).send(viewBills)
     //View bills
 }
 exports.generateBill = async (req, res) => {
     //Generate bills
 }
 exports.closeTable = async (req, res) => {
-    //Close the tables for the bills pending
+    exports.closeTable = async function (req, res) {
+        var Table = table.findByIdAndDelete(req.params.string)
+        if (!table) {
+            res.status(404).send(`bill with id ${req.params.id} doesn't exist`)
+        }
+    
+        res.status(200).send({ message: "closed" })
+    }
+    
+    
 }
 exports.modeofPayment = async (req, res) => {
     //Send the mode of payment with the order id
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,36 @@
+const Token = require("../models/tokern.dmodel")
+const PasswordHash = require("password-hash")
+exports.signup = async (req, res) => {
+    //Register the restaurant
+}
 exports.signin = async (req, res) => {
-    //Sign in with existing account
+    // First check if account exists with this email id
+  var user = await User.findOne({ email: req.body.email })
+  if (!user) {
+    // 404 Not Found
+    return res.status(404).send({ message: "Account does not exists" })
+  }
+
+  // verify password
+  if (!PasswordHash.verify(req.body.password, user.password)) {
+    // 403 : Forbidden
+    return res.status(403).send({ message: "Invalid password" })
+  }
+
+  // create new token
+  var token = new Token({ userId: user._id })
+
+  // save token in database
+  token = await token.save()
+
+  res.header("authorization", token._id)
+  res.status(200).send(user)
 }
 exports.signout = async (req, res) => {
-    //Sign of the account
+    // Remove token from database
+    var token = await Token.findByIdAndDelete(req.token._id)
+    console.log(token)
+    res.status(200).send({ message: "Signout success" })
 }
 exports.retrieveUser = async (req, res) => {
     //Send user associated with the userid
