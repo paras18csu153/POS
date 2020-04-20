f8579e163babd9b0b9a72a47ea2601e1ef6e596b
chirgmalik <46554055+chirgmalik@users.noreply.github.com>
2020-04-20 13:36:16 +0530

    chirag's contribution

--- routes/cashier.route.js
+++ routes/cashier.route.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const cashier_controller = require("../controllers/cashier.controller")
-router.get("/Bills", cashier_controller.viewBills)
-router.post("/Bill", cashier_controller.generateBill)
-router.post("/Payment", cashier_controller.modeofPayment)
-module.exports = router;
+
+
+// Get all bills
+router.get("/", cashier_controller.viewbills)
+// Delete bill
+router.this.post("/:id", cashier_controller.closetable)
+
+
+module.exports = router
