30f24db507dbfad97e587ac4a0afd530334d11a1
marora10 <46513213+marora10@users.noreply.github.com>
2020-05-06 02:17:06 +0530

    cashier and waiter router objects

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ mongoose
 var indexRouter = require('./routes/index.route');
 var userRouter = require('./routes/user.route');
 var ownerRouter=require('./routes/owner.route');
+var waiterRouter = require('./routes/waiter.route');
+var cashierRouter = require('./routes/cashier.route');
 
 var app = express();
 
@@ -42,6 +44,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 app.use('/owner', ownerRouter);
+app.use('/waiter', waiterRouter);
+app.use('/waiter/tables', waiterRouter);
+app.use('/waiter/order', waiterRouter);
+app.use('/waiter/bill', waiterRouter);
+app.use('/waiter/menu', waiterRouter);
+app.use('/cashier/bill', cashierRouter);
+app.use('/cashier/:id', cashierRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
