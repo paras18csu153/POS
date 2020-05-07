a84e76c329e238f74dc812453f1e5792b01dcbb5
paras18csu153 <46554048+paras18csu153@users.noreply.github.com>
2020-05-07 10:09:34 +0530

    Integrated

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,24 @@
+var mongoose = require('mongoose');
 const Token = require("../models/token.model");
 
 module.exports = async function(req, res, next) {
-  const tokenId = req.headers["authorization"];
+  var id=req.headers["authorization"];
+  id=id.substring(14,id.length);
+  var id = mongoose.Types.ObjectId(id);
+  const tokenId = id;
   if (!tokenId) {
     // 401 : Unauthorized
     return res.status(401).send({ message: "No token provided" });
   }
-
   var token = await Token.findById(tokenId);
   if (!token) {
     // 403 : Forbidden
     return res.status(403).send({ message: "Invalid token" });
   }
-
   // set token in request for further use
   // this creates a new key called token and add's it to req json
   req.token = token;
-
+  console.log(",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,")
   // make sure to call next
   next();
 };
