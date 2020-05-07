cf114378c1406ee278dd6c24a5d1ae73577e5d1b
paras18csu153 <46554048+paras18csu153@users.noreply.github.com>
2020-05-07 10:12:19 +0530

    Modified

--- models/dish.model.js
+++ models/dish.model.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema.Types;
 let dishSchema = new mongoose.Schema({
 dishid: { type: ObjectId, ref: "Owner" },
+dishimage: String,
 dishname:String,
-vegnonveg:Boolean,
-category:String
+vegnonveg:String,
+category:String,
+price:Number
 })
 
 module.exports= mongoose.model('Dish',dishSchema)
--- models/token.model.js
+++ models/token.model.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema.Types;
 
 let tokenSchema = new mongoose.Schema({
-  ownerId: { type: ObjectId, ref: "Owner" }
+  ownerId: { type: ObjectId, ref: "Owner" },
+  userId: { type: ObjectId, ref: "User" }
 });
 
 module.exports = mongoose.model("Token", tokenSchema);
